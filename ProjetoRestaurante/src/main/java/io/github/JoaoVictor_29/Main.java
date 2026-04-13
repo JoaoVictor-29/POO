3683f72e6647048daec674f6119c95152ac8b6b4
JoaoVictor-29 <jvrp1000@gmail.com>
2026-04-13 18:58:14 -0300

    Iniciando projeto Restaurante

---
+++ ProjetoRestaurante/src/main/java/io/github/JoaoVictor_29/Main.java
@@ -0,0 +1,9 @@
+package io.github.JoaoVictor_29;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+
+    }
+}
